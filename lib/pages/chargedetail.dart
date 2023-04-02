131d2778d14dea1121b19782ca31594dfb9ead29
Nhooearn <99580726+Nhooearn@users.noreply.github.com>
2023-04-03 01:52:41 +0700

    ui_chargedetail

--- lib/pages/chargedetail.dart
+++ lib/pages/chargedetail.dart
@@ -1,9 +1,15 @@
 import 'package:chat_test/pages/map.dart';
 import 'package:chat_test/pages/payment.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../widgets/widgets.dart';
+import 'addcar.dart';
+import 'mycar.dart';
 
 class ChargeDetail extends StatefulWidget {
   const ChargeDetail({super.key});
@@ -13,9 +19,12 @@ class ChargeDetail extends StatefulWidget {
 }
 
 class _ChargeDetailState extends State<ChargeDetail> {
+  final List<Map<String, dynamic>> _car = [];
+  final userId = FirebaseAuth.instance.currentUser!.uid;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff292929),
       appBar: AppBar(
         title: Text('Charge',
             style:
@@ -33,6 +42,148 @@ class _ChargeDetailState extends State<ChargeDetail> {
             // ฟังก์ชันที่จะทำงานเมื่อกดปุ่มย้อนกลับ
           },
         ),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("car")
+            .where("owner", isEqualTo: userId)
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 430,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "ยังไม่ได้เพิ่มรถ\nกรุณาเพิ่มรถของคุณ",
+                        style: GoogleFonts.prompt(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      nextScreenReplace(context, const addCarScreen());
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Icon(Icons.add),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 450,
+                  child: ListView.builder(
+                    itemCount: snapshot.data?.docs.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                        ),
+                        margin: const EdgeInsets.fromLTRB(35, 10, 10, 10),
+                        child: Column(
+                          children: [
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/images/car.png",
+                                  width: 260,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Column(
+                                  children: [
+                                    Text(
+                                      snapshot.data!.docs[index]['carId'],
+                                      style: GoogleFonts.prompt(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 24,
+                                          color: Colors.black),
+                                    ),
+                                    Text(
+                                      snapshot.data!.docs[index]['provinces'],
+                                      style: GoogleFonts.prompt(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 24,
+                                          color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                // const SizedBox(
+                //   height: 5,
+                // ),
+                // GestureDetector(
+                //   onTap: () {
+                //     nextScreenReplace(context, const addCarScreen());
+                //   },
+                //   child: Container(
+                //     alignment: Alignment.center,
+                //     height: 50,
+                //     width: 200,
+                //     decoration: BoxDecoration(
+                //       color: Colors.green,
+                //       borderRadius: BorderRadius.circular(30),
+                //     ),
+                //     child: Text(
+                //       'ยืนยัน',
+                //       style: GoogleFonts.prompt(
+                //           fontWeight: FontWeight.w400,
+                //           fontSize: 16,
+                //           color: Colors.black),
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
+          );
+        },
       ),
       floatingActionButton: Container(
         height: 70,
